aeb0c7142d7989cdf15b1b7a9ff231afb95e6be1
Pramudit <spramudit@gmail.com>
2017-12-13 01:05:29 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,51 @@
+
+
+const APIAI_TOKEN = process.env.APIAI_TOKEN;
+const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // js, css, images
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+const apiai = require('apiai')("ac3a6d680e8d4b7ba50807394ca51da5");
+
+// Web UI
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket) {
+  socket.on('chat message', (text) => {
+    console.log('Message: ' + text);
+
+    // Get a reply from API.ai
+
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: "258f85d7209f499aa41e5a725ba3d1d7"
+    });
+
+    apiaiReq.on('response', (response) => {
+      let aiText = response.result.fulfillment.speech;
+      console.log('Bot reply: ' + aiText);
+      socket.emit('bot reply', aiText);
+    });
+
+    apiaiReq.on('error', (error) => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+
+  });
+});
